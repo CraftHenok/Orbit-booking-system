8598e1b468e89d72049eb95b4348aa4ec1453eb8
zekaryas <zekaryasdinku@gmail.com>
2020-02-16 23:46:43 -1200

    fetching from remote added(only for get doctor and patient)

--- src/app/services/Doctors/doctors.service.ts
+++ src/app/services/Doctors/doctors.service.ts
@@ -1,32 +1,35 @@
 import {Injectable} from '@angular/core';
 import {Doctor} from '../../models/Doctor';
-import {of} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DoctorsService {
 
-  constructor() {
+  constructor(private http: HttpClient) {
   }
 
   getAllDoctors() {
 
-    const doctorsList: Doctor[] = [];
+    // const doctorsList: Doctor[] = [];
+    //
+    // doctorsList.push(new Doctor(1, 'Zekaryas tadele 1', 'Zekaryas 1', '1234', 1,
+    //   false, true, true));
+    // doctorsList.push(new Doctor(2, 'Zekaryas tadele 2', 'Zekaryas 2', '1234', 2,
+    //   false, false, false));
+    // doctorsList.push(new Doctor(3, 'Zekaryas tadele 3', 'Zekaryas 3', '1234', 3,
+    //   false, true, true));
+    // doctorsList.push(new Doctor(4, 'Zekaryas tadele 4', 'Zekaryas 4', '1234', 4,
+    //   false, false, false));
+    // doctorsList.push(new Doctor(5, 'Zekaryas tadele 5', 'Zekaryas 5', '1234', 5,
+    //   false, true, true));
+    // doctorsList.push(new Doctor(6, 'Zekaryas tadele 6', 'Zekaryas 6', '1234', 6,
+    //   false, false, false));
+    //
+    // return of(doctorsList);
+    const url = 'http://localhost:5000/doctor/';
+    return this.http.get<Doctor[]>(url);
 
-    doctorsList.push(new Doctor(1, 'Zekaryas tadele 1', 'Zekaryas 1', '1234', 1,
-      false, true, true));
-    doctorsList.push(new Doctor(2, 'Zekaryas tadele 2', 'Zekaryas 2', '1234', 2,
-      false, false, false));
-    doctorsList.push(new Doctor(3, 'Zekaryas tadele 3', 'Zekaryas 3', '1234', 3,
-      false, true, true));
-    doctorsList.push(new Doctor(4, 'Zekaryas tadele 4', 'Zekaryas 4', '1234', 4,
-      false, false, false));
-    doctorsList.push(new Doctor(5, 'Zekaryas tadele 5', 'Zekaryas 5', '1234', 5,
-      false, true, true));
-    doctorsList.push(new Doctor(6, 'Zekaryas tadele 6', 'Zekaryas 6', '1234', 6,
-      false, false, false));
-
-    return of(doctorsList);
   }
 }
