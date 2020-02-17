e2ad0b4773d1f978fe21a4f8c6b5d6842b67686a
zekaryas <zekaryasdinku@gmail.com>
2020-02-17 00:31:52 -1200

    added address section

--- src/app/components/patients/patients.component.ts
+++ src/app/components/patients/patients.component.ts
@@ -23,7 +23,7 @@ export class PatientsComponent implements OnInit, OnDestroy {
 
   dataSource;
   displayedColumns: string[] = ['seq', 'regDate', 'active', 'name', 'gender', 'dateOfBirth', 'nationality'
-    , 'contactInfo', 'emergencyInfo', 'edit', 'delete'];
+    , 'contactInfo', 'address', 'emergencyInfo', 'edit', 'delete'];
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
   private subscription: Subscription = new Subscription();
