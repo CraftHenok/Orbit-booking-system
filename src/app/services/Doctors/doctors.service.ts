137a0b543702cd3013b60faa3f8f8305c78e847a
zekaryas <zekaryasdinku@gmail.com>
2020-02-20 02:14:18 -1200

    cached doctor http response

--- src/app/services/Doctors/doctors.service.ts
+++ src/app/services/Doctors/doctors.service.ts
@@ -1,35 +1,28 @@
 import {Injectable} from '@angular/core';
 import {Doctor} from '../../models/Doctor';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {shareReplay} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DoctorsService {
 
+  private doctors$: Observable<Doctor[]>;
+
   constructor(private http: HttpClient) {
   }
 
   getAllDoctors() {
 
-    // const doctorsList: Doctor[] = [];
-    //
-    // doctorsList.push(new Doctor(1, 'Zekaryas tadele 1', 'Zekaryas 1', '1234', 1,
-    //   false, true, true));
-    // doctorsList.push(new Doctor(2, 'Zekaryas tadele 2', 'Zekaryas 2', '1234', 2,
-    //   false, false, false));
-    // doctorsList.push(new Doctor(3, 'Zekaryas tadele 3', 'Zekaryas 3', '1234', 3,
-    //   false, true, true));
-    // doctorsList.push(new Doctor(4, 'Zekaryas tadele 4', 'Zekaryas 4', '1234', 4,
-    //   false, false, false));
-    // doctorsList.push(new Doctor(5, 'Zekaryas tadele 5', 'Zekaryas 5', '1234', 5,
-    //   false, true, true));
-    // doctorsList.push(new Doctor(6, 'Zekaryas tadele 6', 'Zekaryas 6', '1234', 6,
-    //   false, false, false));
-    //
-    // return of(doctorsList);
     const url = 'http://localhost:5000/doctor/';
-    return this.http.get<Doctor[]>(url);
+    if (!this.doctors$) {
+      this.doctors$ = this.http.get<Doctor[]>(url).pipe(
+        shareReplay(1)
+      );
+    }
+    return this.doctors$;
 
   }
 }
