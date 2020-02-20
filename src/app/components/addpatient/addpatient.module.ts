b85e3e9ce8203f8f803dcab2ce4fafe7d4792e6e
zekaryas <zekaryasdinku@gmail.com>
2020-02-20 02:12:37 -1200

    add patient all form added

--- src/app/components/addpatient/addpatient.module.ts
+++ src/app/components/addpatient/addpatient.module.ts
@@ -3,6 +3,13 @@ import {CommonModule} from '@angular/common';
 
 import {AddpatientRoutingModule} from './addpatient-routing.module';
 import {AddpatientComponent} from './addpatient.component';
+import {MatStepperModule} from '@angular/material/stepper';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 
 
 @NgModule({
@@ -11,7 +18,14 @@ import {AddpatientComponent} from './addpatient.component';
   ],
   imports: [
     CommonModule,
-    AddpatientRoutingModule
+    AddpatientRoutingModule,
+    MatStepperModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatAutocompleteModule
   ]
 })
 export class AddpatientModule { }
