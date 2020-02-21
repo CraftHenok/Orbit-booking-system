cc279dbeb778b8218f3ca34d8259eaa342d00714
zekaryas <zekaryasdinku@gmail.com>
2020-02-20 23:18:36 -1200

    add doctor ui- same as add patient format

--- src/app/components/adddoctor/adddoctor.component.ts
+++ src/app/components/adddoctor/adddoctor.component.ts
@@ -1,13 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-adddoctor',
   templateUrl: './adddoctor.component.html',
-  styleUrls: ['./adddoctor.component.css']
+  styleUrls: ['../addpatient/addpatient.component.css'] // reuse
 })
 export class AdddoctorComponent implements OnInit {
 
-  constructor() { }
+  hidePassword = true;
+
+  primaryInfo = this.formBuilder.group({
+    name: ['', Validators.required],
+    username: ['', Validators.required],
+    password: ['', [Validators.required, Validators.min(6)]]
+  });
+
+  appointmentRelatedInfo = this.formBuilder.group({
+    displayOrder: ['', Validators.required],
+    manageBlock: [false, Validators.required],
+    manageBooking: [false, Validators.required],
+    isDoctor: [false, Validators.required]
+  });
+
+
+  constructor(private formBuilder: FormBuilder) {
+  }
 
   ngOnInit(): void {
   }
--- src/app/components/adddoctor/adddoctor.module.ts
+++ src/app/components/adddoctor/adddoctor.module.ts
@@ -3,6 +3,13 @@ import {CommonModule} from '@angular/common';
 
 import {AdddoctorRoutingModule} from './adddoctor-routing.module';
 import {AdddoctorComponent} from './adddoctor.component';
+import {MatStepperModule} from '@angular/material/stepper';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 
 @NgModule({
@@ -11,7 +18,14 @@ import {AdddoctorComponent} from './adddoctor.component';
   ],
   imports: [
     CommonModule,
-    AdddoctorRoutingModule
+    AdddoctorRoutingModule,
+    MatStepperModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCheckboxModule
   ]
 })
 export class AdddoctorModule {
