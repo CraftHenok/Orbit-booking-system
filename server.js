67ecb7885d2862fd8eb7e7b847f55b5d815a27c0
zekaryas <zekaryasdinku@gmail.com>
2020-02-27 21:53:35 -1200

    pwa support added but still need https

--- server.js
+++ server.js
@@ -1,45 +1,51 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const migrationManager = require('./backend/utitlity/migrationManager');
-const stateManager = require("./backend/utitlity/stateManager");
+// const migrationManager = require('./backend/utitlity/migrationManager');
+// const stateManager = require("./backend/utitlity/stateManager");
+const compression = require('compression');
+
+
+//compress what you serve
+app.use(compression());
 
 
 // Body Parser Middleware
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+// app.use(express.json());
+// app.use(express.urlencoded({extended: false}));
 
 // cors
 app.use(cors());
 
 
-if (stateManager.createTables()) {
-  //create tables
-  console.log("Need to create tables");
-  migrationManager.doMigrationThenSeeding();
-  stateManager.createFile();
-}
+// if (stateManager.createTables()) {
+//   //create tables
+//   console.log("Need to create tables");
+//   migrationManager.doMigrationThenSeeding();
+//   stateManager.createFile();
+// }
 
 
 // In case there comes a need to migrate data without file use
 // don't run this if there are already table or don't know what you are doing
-app.get("/doManualMigration/OrbitHealth", () => {
-  migrationManager.doMigrationThenSeeding();
-  stateManager.createFile();
+// app.get("/doManualMigration/OrbitHealth", () => {
+//   migrationManager.doMigrationThenSeeding();
+//   stateManager.createFile();
+// });
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/bookingsystem'));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/bookingsystem/index.html'));
 });
-
-
-app.get('/', function (req, res) {
-  res.json('Hello World');
-});
-
 
 // available api routes
-app.use('/appointment', require('./backend/route/appointmetsRoute'));
-app.use('/doctor', require('./backend/route/doctorRoute'));
-app.use('/patient', require('./backend/route/patientRoute'));
+// app.use('/appointment', require('./backend/route/appointmetsRoute'));
+// app.use('/doctor', require('./backend/route/doctorRoute'));
+// app.use('/patient', require('./backend/route/patientRoute'));
 
 
 //check if we need to create
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
