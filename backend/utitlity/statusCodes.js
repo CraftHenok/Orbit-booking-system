a0fd64a05e02ecd3c1ddddaae41c5a342d4ec1f5
zekaryas <zekaryasdinku@gmail.com>
2020-03-18 00:28:00 -1200

    created status code module to easily change and use

---
+++ backend/utitlity/statusCodes.js
@@ -0,0 +1,14 @@
+exports.statusCode = (function () {
+  return {
+    saveOk: 201,
+    getOk: 200,
+    updateOKNoData: 204,
+    updateOkData: 200,
+    unAuthorized: 401,
+    forbidden: 403,
+    notFound: 404,
+    errorInData: 400
+  };
+})();
+
+
