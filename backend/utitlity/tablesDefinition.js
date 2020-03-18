b83ea145b62fe7a47e601c244b531679453956e1
zekaryas <zekaryasdinku@gmail.com>
2020-03-17 20:22:12 -1200

    updated table definition to the latest working version

--- backend/utitlity/tablesDefinition.js
+++ backend/utitlity/tablesDefinition.js
@@ -1,20 +1,23 @@
-const appointmentTable = "CREATE TABLE \"Appointment\" (\n" +
+const appointment = "CREATE TABLE \"Appointment\" (\n" +
   "\t\"patientId\"\tINTEGER NOT NULL,\n" +
   "\t\"appointmentTypeId\"\tINTEGER NOT NULL,\n" +
   "\t\"appointmentStatusId\"\tINTEGER NOT NULL,\n" +
   "\t\"startDateTime\"\tTEXT NOT NULL,\n" +
   "\t\"endDateTime\"\tTEXT NOT NULL,\n" +
   "\t\"isServed\"\tINTEGER,\n" +
-  "\t\"servedBy\"\tINTEGER\n" +
+  "\t\"servedBy\"\tINTEGER,\n" +
   "\t\"userId\"\tINTEGER NOT NULL,\n" +
+  "\t\"id\"\tINTEGER PRIMARY KEY AUTOINCREMENT\n" +
   ");";
 
 const appointmentType = "CREATE TABLE \"appointmentType\" (\n" +
-  "\t\"type\"\tTEXT NOT NULL\n" +
+  "\t\"type\"\tTEXT NOT NULL,\n" +
+  "\t\"id\"\tINTEGER PRIMARY KEY AUTOINCREMENT\n" +
   ");";
 
 const appointmentStatus = "CREATE TABLE \"appointmentStatus\" (\n" +
-  "\t\"status\"\tTEXT NOT NULL\n" +
+  "\t\"status\"\tTEXT NOT NULL,\n" +
+  "\t\"id\"\tINTEGER PRIMARY KEY AUTOINCREMENT\n" +
   ");";
 
 const patient = "CREATE TABLE \"Patient\" (\n" +
@@ -30,35 +33,41 @@ const patient = "CREATE TABLE \"Patient\" (\n" +
   "\t\"contactId\"\tINTEGER NOT NULL,\n" +
   "\t\"addressId\"\tINTEGER NOT NULL,\n" +
   "\t\"regDate\"\tTEXT NOT NULL,\n" +
-  "\t\"emergencyInfoId\"\tINTEGER NOT NULL\n" +
+  "\t\"emergencyInfoId\"\tINTEGER NOT NULL,\n" +
+  "\t\"id\"\tINTEGER PRIMARY KEY AUTOINCREMENT\n" +
   ");";
 
 const patientTitle = "CREATE TABLE \"PatientTitle\" (\n" +
-  "\t\"title\"\tTEXT NOT NULL\n" +
+  "\t\"title\"\tTEXT NOT NULL,\n" +
+  "\t\"id\"\tINTEGER PRIMARY KEY AUTOINCREMENT\n" +
   ");";
 
 const contact = "CREATE TABLE \"Contact\" (\n" +
   "\t\"email\"\tTEXT,\n" +
   "\t\"phoneNumber\"\tTEXT,\n" +
-  "\t\"alternatePhoneNumber\"\tTEXT\n" +
+  "\t\"alternatePhoneNumber\"\tTEXT,\n" +
+  "\t\"id\"\tINTEGER PRIMARY KEY AUTOINCREMENT\n" +
   ");";
 
 const address = "CREATE TABLE \"Address\" (\n" +
   "\t\"line1\"\tTEXT,\n" +
   "\t\"line2\"\tTEXT,\n" +
   "\t\"city\"\tTEXT,\n" +
-  "\t\"country\"\tTEXT\n" +
+  "\t\"country\"\tTEXT,\n" +
+  "\t\"id\"\tINTEGER PRIMARY KEY AUTOINCREMENT\n" +
   ");";
 
 const emergencyContact = "CREATE TABLE \"EmergencyContact\" (\n" +
   "\t\"emergencyTitleId\"\tINTEGER,\n" +
   "\t\"name\"\tTEXT,\n" +
   "\t\"phoneNumber\"\tTEXT,\n" +
-  "\t\"alternatePhoneNumber\"\tTEXT\n" +
+  "\t\"alternatePhoneNumber\"\tTEXT,\n" +
+  "\t\"id\"\tINTEGER PRIMARY KEY AUTOINCREMENT\n" +
   ");";
 
 const emergencyTitle = "CREATE TABLE \"emergencyTitle\" (\n" +
-  "\t\"title\"\tTEXT NOT NULL\n" +
+  "\t\"title\"\tTEXT NOT NULL,\n" +
+  "\t\"id\"\tINTEGER PRIMARY KEY AUTOINCREMENT\n" +
   ");";
 
 const doctor = "CREATE TABLE \"Doctor\" (\n" +
@@ -67,21 +76,24 @@ const doctor = "CREATE TABLE \"Doctor\" (\n" +
   "\t\"displayOrder\"\tINTEGER,\n" +
   "\t\"manageBlocks\"\tINTEGER NOT NULL,\n" +
   "\t\"manageBooking\"\tINTEGER NOT NULL,\n" +
-  "\t\"isDoctor\"\tINTEGER NOT NULL\n" +
+  "\t\"isDoctor\"\tINTEGER NOT NULL,\n" +
+  "\t\"id\"\tINTEGER PRIMARY KEY AUTOINCREMENT\n" +
   ");";
 
 const duration = "CREATE TABLE \"duration\" (\n" +
-  "\t\"duration\"\tINTEGER NOT NULL\n" +
+  "\t\"duration\"\tINTEGER NOT NULL,\n" +
+  "\t\"id\"\tINTEGER PRIMARY KEY AUTOINCREMENT\n" +
   ");";
 
 const user = "CREATE TABLE \"User\" (\n" +
   "\t\"email\"\tTEXT NOT NULL UNIQUE,\n" +
   "\t\"password\"\tTEXT NOT NULL,\n" +
-  "\t\"role\"\tTEXT NOT NULL\n" +
+  "\t\"role\"\tTEXT NOT NULL,\n" +
+  "\t\"id\"\tINTEGER PRIMARY KEY AUTOINCREMENT\n" +
   ");";
 
 module.exports = {
-  "appointmentTable": appointmentTable,
+  "appointmentTable": appointment,
   "appointmentType": appointmentType,
   "appointmentStatus": appointmentStatus,
   "patient": patient,
