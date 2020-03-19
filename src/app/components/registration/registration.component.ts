d430b913cc00e5d3e36baf6d598d1abe459a4032
zekaryas <zekaryasdinku@gmail.com>
2020-03-19 00:57:44 -1200

    registration page

---
+++ src/app/components/registration/registration.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['../login/login.component.css']
+})
+export class RegistrationComponent implements OnInit {
+
+  hide = true;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
