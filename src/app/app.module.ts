0c660cfe9920732ee77e7fd72f8fcc5c0f3c7e67
zekaryas <zekaryasdinku@gmail.com>
2020-03-25 22:10:36 -1200

    removed unused imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -10,29 +9,22 @@ import {AddEditDialogComponent} from './components/dialogs/addEditDialog/addEdit
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {ServiceWorkerModule} from '@angular/service-worker';
 import {environment} from '../environments/environment';
-import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {MatDialogModule} from '@angular/material/dialog';
-import {MatTableModule} from '@angular/material/table';
 import {MatInputModule} from '@angular/material/input';
 import {HttpClientModule} from '@angular/common/http';
-import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
 import {MatSelectModule} from '@angular/material/select';
 import {OwlDateTimeModule, OwlNativeDateTimeModule} from 'ng-pick-datetime';
 import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatRippleModule} from '@angular/material/core';
-import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatChipsModule} from '@angular/material/chips';
 import {ForgetIdComponent} from './components/dialogs/forget-id/forget-id.component';
 import {QuickAddComponent} from './components/dialogs/quick-add/quick-add.component';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {NgxSpinnerModule} from 'ngx-spinner';
 import {HomepageComponent} from './components/homepage/homepage.component';
 import {LoginComponent} from './components/login/login.component';
-import {MatCardModule} from '@angular/material/card';
 import {JwtModule} from '@auth0/angular-jwt';
+import {MatIconModule} from '@angular/material/icon';
 
 // ng g module moduleName --flat --routing
 
@@ -53,17 +45,15 @@ export function tokenGetter() {
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
     MatButtonModule,
     OwlDateTimeModule,
     OwlNativeDateTimeModule,
-    MatButtonToggleModule,
+    MatIconModule,
     MatInputModule,
     CalendarModule.forRoot({provide: DateAdapter, useFactory: adapterFactory}),
     MatDialogModule,
     ReactiveFormsModule,
     ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
-    MatTableModule,
     HttpClientModule,
     JwtModule.forRoot({
       config: {
@@ -73,17 +63,12 @@ export function tokenGetter() {
         blacklistedRoutes: ['localhost:8080/account/login', 'localhost:8080/account/register']
       }
     }),
-    MatIconModule,
     MatSelectModule,
     MatListModule,
     MatCheckboxModule,
-    MatRippleModule,
-    MatTooltipModule,
     FormsModule,
     MatChipsModule,
-    MatProgressSpinnerModule,
     NgxSpinnerModule,
-    MatCardModule
   ],
   entryComponents: [
     AddEditDialogComponent,
