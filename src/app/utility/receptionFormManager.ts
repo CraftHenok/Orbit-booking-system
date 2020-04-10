962e4ec7a62a110de139fa80d89016b89f312609
zekaryas <zekaryasdinku@gmail.com>
2020-04-10 01:01:29 -1200

    the common form-manager for reception edit and add component

---
+++ src/app/utility/receptionFormManager.ts
@@ -0,0 +1,49 @@
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {Account} from '../models/Account';
+import {Variables} from './variables';
+
+export class ReceptionFormManager {
+
+  private ReceptionForm: FormGroup;
+
+
+  get receptionForm(): FormGroup {
+    return this.ReceptionForm;
+  }
+
+  set receptionForm(value: FormGroup) {
+    this.ReceptionForm = value;
+  }
+
+
+  updateForm(account: Account) {
+    this.receptionForm.patchValue({
+      username: account.username,
+      email: account.email,
+      password: account.password,
+      status: account.status,
+    });
+  }
+
+  constructor(private formBuilder: FormBuilder) {
+    this.ReceptionForm = this.getFormBuilder();
+  }
+
+  bindToNewReception() {
+    return new Account(
+      this.receptionForm.get('email').value,
+      this.receptionForm.get('password').value,
+      Variables.receptionRoleName,
+      this.receptionForm.get('username').value,
+      this.receptionForm.get('status').value);
+  }
+
+  private getFormBuilder() {
+    return this.formBuilder.group({
+      username: [''],
+      email: [''],
+      password: [''],
+      status: [''],
+    });
+  }
+}
