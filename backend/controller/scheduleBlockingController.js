18921d78bb93d0ae83568a2c52c1df1b32388f76
zekaryas <zekaryasdinku@gmail.com>
2020-04-13 18:40:27 -1200

    delete return the number of affected rows

--- backend/controller/scheduleBlockingController.js
+++ backend/controller/scheduleBlockingController.js
@@ -39,7 +39,7 @@ exports.deleteScheduleBlocking = async (req, res) => {
     if (err) {
       res.json(err.message).status(statusCode.notFound);
     } else {
-      res.status(statusCode.deleteOk).json(this.changes);
+      res.json(this.changes).status(statusCode.deleteOk);
     }
   });
 };
