ca343bc9cb9580e5314813d2324fec4995409277
zekaryas <zekaryasdinku@gmail.com>
2020-04-13 18:43:20 -1200

    one function is enough

--- src/app/utility/settingDialogData.ts
+++ src/app/utility/settingDialogData.ts
@@ -1,18 +1,10 @@
 export class SettingDialogData {
 
-  static prepareForOld(id: any, value: any, dataName: string) {
-    const map: Map<string, string> = new Map();
-    map.set('id', id);
+  static prepare(dataName: string, id?: number, value?: any) {
+    const map = new Map();
+    map.set('id', id || '0');
     map.set('dataName', dataName);
-    map.set('value', value);
-    return map;
-  }
-
-  static prepareForNew(dataName: string) {
-    const map: Map<string, string> = new Map();
-    map.set('id', '0');
-    map.set('dataName', dataName);
-    map.set('value', '');
+    map.set('value', value || '');
     return map;
   }
 }
