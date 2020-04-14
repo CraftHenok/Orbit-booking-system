4f38fdbca255d2f8da4b558b2eada0d3cf060672
zekaryas <zekaryasdinku@gmail.com>
2020-04-13 18:42:36 -1200

    all colors should be placed here and changed name to more meaningful

--- src/app/models/Color.ts
+++ src/app/models/Color.ts
@@ -1,15 +1,19 @@
 const colors: any = {
-  red: {
+  pastUnserved: {
     primary: '#ad2121',
     secondary: '#FAE3E3'
   },
-  blue: {
+  pastServed: {
     primary: '#1e90ff',
     secondary: '#D1E8FF'
   },
-  yellow: {
+  future: {
     primary: '#e3bc08',
     secondary: '#FDF1BA'
+  },
+  scheduleBlocking: {
+    primary: '#123',
+    secondary: '#123'
   }
 };
 
